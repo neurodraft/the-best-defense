5db1d7f6d760d0059e44ccbf93de9890ff8b1a8d
JoaoMendes23 <73128559+JoaoMendes23@users.noreply.github.com>
2021-07-04 16:50:39 +0100

    Blade start

---
+++ Assets/BladeDamage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision with player");
+            EventManager.TriggerEvent("player_damage", new Dictionary<string, object> { { "amount", 20 } });
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision with player Trigger");
+            EventManager.TriggerEvent("player_damage", new Dictionary<string, object> { { "amount", 20 } });
+        }
+    }
+}
---
+++ Assets/BladeTrap.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeTrap : MonoBehaviour
+{
+    /*public Transform blade;
+    public float bladeDuration = 5f;
+    public float cooldown = 5f;
+    private bool activated = false;
+    private float cooldownTimer = 0f;*/
+
+    public float speed = 1f;
+    public float maxRotation = 45f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, maxRotation * Mathf.Sin(Time.time * speed));
+        /*if (!activated)
+        {
+            cooldownTimer += Time.deltaTime;
+            if (cooldownTimer >= cooldown)
+            {
+                StartCoroutine(bladeRoutine());
+                cooldownTimer = 0;
+            }
+        }*/
+    }
+
+    /*private IEnumerator bladeRoutine()
+    {
+        activated = true;
+        float timer = 0f;
+        Vector3 targetPosition = blade.position + Vector3.up * 1.75f;
+        Vector3 originalPosition = blade.position;
+
+        while (timer < bladeDuration)
+        {
+            timer += Time.fixedDeltaTime;
+            float fraction = timer / bladeDuration;
+            float weight = Mathf.Sin(Mathf.PI * fraction);
+            blade.position = Vector3.Lerp(originalPosition, targetPosition, weight);
+            yield return new WaitForFixedUpdate();
+        }
+
+        activated = false;
+    }*/
+}
