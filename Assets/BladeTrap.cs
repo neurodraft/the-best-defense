9710ca356078aa95610fa70768eb503745aa771c
JoaoMendes23 <73128559+JoaoMendes23@users.noreply.github.com>
2021-07-05 16:49:25 +0100

    Blade Trap trigger with bug
    Bug: When the blade hits the player, he will be pushed to the opposite direction of where he is facing. So that means if the blade hits him when he is with his back agains the blade, he will be pushed into the blade

---
+++ Assets/BladeTrap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeTrap : MonoBehaviour
+{
+    public int maxRotationDegree = 45;
+    public float bladeSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, maxRotationDegree * Mathf.Sin(Time.time * bladeSpeed));
+    }
+}
---
+++ Assets/BladeTrapDamage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeTrapDamage : MonoBehaviour
+{
+    public float bladeDamage = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Blade collision player");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Blade trigger player");
+            Vector3 direction = other.gameObject.transform.forward * -1;
+            Vector3 position = other.gameObject.transform.position;
+            EventManager.TriggerEvent("player_damage", new Dictionary<string, object> { { "amount", 10 }, { "direction", direction }, { "position", position } });
+        }
+    }
+}
